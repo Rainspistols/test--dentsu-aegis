bc346c4832798fb8fc13b1916fae200e9516ba8b
Rainspistols <ffly@mail.ru>
2020-07-28 16:47:12 +0200

    Updates

---
+++ precache-manifest.fabcdf75c05a103feb14be6559351ca3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bb300a3dbfd6d0839353940db28e28",
+    "url": "/test--dentsu-aegis/index.html"
+  },
+  {
+    "revision": "70e37c9aadf46455e33f",
+    "url": "/test--dentsu-aegis/static/js/2.cc1fb244.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test--dentsu-aegis/static/js/2.cc1fb244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4cc2e566b6451966d8",
+    "url": "/test--dentsu-aegis/static/js/main.941c891d.chunk.js"
+  },
+  {
+    "revision": "4a80b693c62b06b91f70",
+    "url": "/test--dentsu-aegis/static/js/runtime-main.145f822a.js"
+  }
+]);
